90ac0d894bcd0795eff5a38e48a9a47e32457832
Anjali Yewate <133753178+Anjali-yewate@users.noreply.github.com>
2024-10-04 13:18:37 +0530

    Add files via upload

---
+++ TTT.js
@@ -0,0 +1,14 @@
+import TicToc from './tic tac toy';
+
+
+function App() {
+  return (
+    <div className="App">
+            <TicToc/>
+      <div>CountAppClass</div>
+      </div> 
+  );
+
+}
+const Message=({userName})=> <h2>welcome {userName}</h2>
+export default TTT;
